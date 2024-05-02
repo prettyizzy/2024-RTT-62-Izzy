866107389bfef997200c0051f2379a7fee2ec526
Isabella Davis <isagdavis@outlook.com>
2024-05-01 20:44:27 -0400

    changed all print statements to 'println()', order receipt formatting and comment formatting

--- java_classwork/JavaClasswork/src/main/java/org/example/Homework303_2_1.java
+++ java_classwork/JavaClasswork/src/main/java/org/example/Homework303_2_1.java
@@ -69,27 +69,27 @@ public class Homework303_2_1 {
         double num2 = 1.5;
 
         double quotientDouble = num1 / num2;
-        System.out.print("The quotient of " + num1 + " and " + num2 + " is " + quotientDouble);
+        System.out.println("The quotient of " + num1 + " and " + num2 + " is " + quotientDouble);
 
         // Now, cast the result to an integer. Print out the result again.
         int quotientInt = (int) quotientDouble;
-        System.out.print("The quotient of " + num1 + " and " + num2 + " as an integer is " + quotientInt);
+        System.out.println("The quotient of " + num1 + " and " + num2 + " as an integer is " + quotientInt);
 
     }
 
     public static void castingOfInts() {
-        // Write a program that declares two integer variables, x and y,
-        // and assigns the number 5 to x and the number 6 to y.
+        // Write a program that declares two integer variables, x and y and assigns
+        // the number 5 to x and the number 6 to y.
         int x = 5;
         int y = 6;
 
         // Declare a variable q and assign y/x to it and print q.
         double q = y/x;
-        System.out.println(q);
+        System.out.println("This is a double: " + q);
 
         // Now, cast y to a double and assign it to q. Print q again.
         q = (double) y;
-        System.out.print(q);
+        System.out.println("This is also a double: " + q);
 
     }
 
@@ -103,6 +103,7 @@ public class Homework303_2_1 {
     }
 
     public static void myCafe() {
+        // Create a constant called SALES_TAX and add sales tax to the subtotal to obtain the totalSale amount.
         double SALES_TAX = 0.06;
 
         // Write a program where you create three variables that represent products at a cafe.
@@ -112,33 +113,24 @@ public class Homework303_2_1 {
         double americano = 3.99;
 
         // Create two more variables called subtotal and totalSale
-        double subtotal = (matchaLatte * 3) + (flatWhite * 4) + (americano * 2);
+        double subtotal = ((matchaLatte * 3) + (flatWhite * 4) + (americano * 2));
         double totalSale = subtotal + (subtotal * SALES_TAX);
 
-        // and complete an “order” for three items of the first product, four items of the second product, and two items of the third product.
+        // Complete an “order” for three items of the first product,
+        // four items of the second product, and two items of the third product.
         System.out.println("""
                 |  ----  ORDER RECEIPT ----  |
                 | MATCHA LATTE.....$5.99 x 3 |
                 | FLAT WHITE.......$4.99 x 4 |
                 | AMERICANO........$3.99 x 2 |
-                -----------
-                """);
-        System.out.println("| SUBTOTAL     " + subtotal + "    |");
-        System.out.println("| SALES TAX     " + SALES_TAX);
-        System.out.println("---------     ");
+                -----------                  |""");
+        System.out.println("| SUBTOTAL            " + subtotal + "  |");
+        System.out.println("| SALES TAX           " + SALES_TAX + "   |");
+        System.out.println("---------                    |");
 
+        // Results formatted to two decimal places
         String formattedSale = String.format("%.2f", totalSale);
-        System.out.println("| TOTAL SALE     " + formattedSale);
-
-
-
-
-
-        // Add them all together to calculate the subtotal.
-        // Create a constant called SALES_TAX and add sales tax to the subtotal to obtain the totalSale amount.
-        // Be sure to format the results to two decimal places.
-
-
+        System.out.println("| TOTAL SALE          " + formattedSale + "  |");
 
     }
 }
