5d142aa3ae58c673ee42c86580fac590d2071a08
Isabella Davis <isagdavis@outlook.com>
2024-05-06 08:53:17 -0400

    adds assignment PA 3035.5.1

---
+++ java_classwork/JavaClasswork/src/main/java/pa305_5_1/GreatestCommonDivisor.java
@@ -0,0 +1,42 @@
+package pa305_5_1;
+
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter the first number: ");
+        int enterFirstNum = scanner.nextInt();
+
+        System.out.print("Please enter the second number: ");
+        int enterSecondNum = scanner.nextInt();
+
+        int secondNumber = 0;
+        int firstNumber = 0;
+
+        if (enterFirstNum > enterSecondNum) {
+            firstNumber = enterSecondNum;
+            secondNumber = enterFirstNum % enterSecondNum;
+            while (secondNumber != 0) {
+                int placeholder = firstNumber;
+                firstNumber = secondNumber;
+                secondNumber = placeholder % secondNumber;
+            }
+        }  else if (enterFirstNum < enterSecondNum) {
+            firstNumber = enterFirstNum;
+            secondNumber = enterSecondNum % enterFirstNum;
+            while (secondNumber != 0) {
+                int placeholder = firstNumber;
+                firstNumber = secondNumber;
+                secondNumber = placeholder % secondNumber;
+            }
+        }
+
+        System.out.println("The GCD of " + enterFirstNum + " and " + enterSecondNum + " is " + firstNumber + ".");
+
+
+
+
+    }
+
+}
---
+++ java_classwork/JavaClasswork/src/main/java/pa305_5_1/PredictFutureTuition.java
@@ -0,0 +1,17 @@
+package pa305_5_1;
+
+public class PredictFutureTuition {
+    public static void main(String[] args) {
+        double tuition = 10000;
+        double doubledTuition = 20000;
+        int years = 0;
+
+        while (tuition <= doubledTuition) {
+            tuition *= 1.07;
+            years++;
+            System.out.println("In " + years + " year(s), the tuition will be: " +  tuition);
+        }
+
+        System.out.println("In " + years + " years, the tuition will have doubled.");
+    }
+}
